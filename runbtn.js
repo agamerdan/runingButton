56266872ec539d77519f2cf924d85ee37b53de1e
Merdan <babayevmerdan20@gmail.com>
2023-04-22 14:39:58 +0300

    add text info

--- runbtn.js
+++ runbtn.js
@@ -1,17 +1,35 @@
 const form=document.querySelector('.form-group')
 const btn=document.querySelector('.btn')
+const info=document.querySelector('.info')
+const span=info.querySelector('span')
+
 form.parola.addEventListener('keyup', e=>{
     const parola="merdan96"
     if(parola==e.target.value){
-        console.log("dogru")
+        span.innerHTML=`<b> Successfull</b>`
+        span.classList.remove('text-danger')
+        span.classList.add('text-success')
+        info.classList.remove('d-none')
     }else{
+        span.innerHTML=`<b>Yuor password is wrong</b>`
+        span.classList.remove('text-success')
+        span.classList.add('text-danger')
+        info.classList.remove('d-none')
         btn.addEventListener('mouseover',e=>{
            if(pos(btn)==835){
-               console.log("ok")
+               
                btn.classList.add('run')
-           }else{
-            btn.classList.remove('run')
            }
+           else if(pos(btn)==1034)
+           {
+                console.log(pos(btn))
+                btn.classList.remove('run')
+                btn.classList.add('runleft')
+            }
+            else
+            {
+                btn.classList.remove('runleft')
+            }
         })
     }
 })
